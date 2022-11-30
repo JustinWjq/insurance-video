afc09423952fdb085587535a232c1cb2a7500cb3
JustinWjq <WjqJustin@163.com>
2022-11-30 18:31:39 +0800

    阳光展业sdk新需求

---
+++ video/src/main/java/com/txt/video/ui/video/plview/ITxPlDisplayViewV3.java
@@ -0,0 +1,23 @@
+package com.txt.video.ui.video.plview;
+
+
+import androidx.annotation.Nullable;
+
+public interface ITxPlDisplayView {
+
+    /**
+     * 横竖屏切换
+     * @param content
+     */
+    public void switchScreen(boolean content);
+
+
+    public void notifyItemChangedPld(int position, @Nullable Object payload);
+
+    public void notifyItemInsertedPld(int position);
+
+    public void notifyItemRemovedPld(int position);
+
+    public void notifyItemChangedPld(int position);
+
+}
